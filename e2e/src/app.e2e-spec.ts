480a4209b3ee5eed52f26018fa241be6b71d499f
gexzor <gexzor@gmail.com>
2021-07-24 21:50:36 +0200

    Renamed from dota-api to warhammer-app

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('dota-api app is running!');
+    expect(await page.getTitleText()).toEqual('warhammer-app app is running!');
   });
 
   afterEach(async () => {
